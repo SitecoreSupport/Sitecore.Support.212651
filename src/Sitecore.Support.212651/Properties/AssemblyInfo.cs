4b2fc5518aac4e18542ccc8e553b37dd2da5d621
DK\ala <art.alexeyenko@gmail.com>
2018-03-19 18:03:56 -0400

    Fist commit. Workaround: hardcode an icon for locked items, so it will be mapped properly

---
+++ src/Sitecore.Support.212651/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.212651")]
+[assembly: AssemblyProduct("Sitecore.Support.212651")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.212651/Shell/Framework/Commands/ContentEditor/SetIcon.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Shell.Framework.Commands.ContentEditor
+{
+	public class SetIcon : Sitecore.Shell.Framework.Commands.ContentEditor.SetIcon
+	{
+		public override string GetIcon(CommandContext context, string defaultValue)
+		{
+			if (this.QueryState(context) == CommandState.Disabled)
+			{
+				return "Custom/16x16/error.png";
+			}
+			if (context.Items.Length == 1)
+			{
+				return context.Items[0].Appearance.Icon;
+			}
+			return base.GetIcon(context, defaultValue);
+		}
+	}
+}
